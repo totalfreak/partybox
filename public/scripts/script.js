0489d27b4c83065652afc567391d615aa3497b49
totalfreak <dgkartin@live.dk>
2016-05-20 23:54:44 +0200

    Redid everything in script.js, reworked the canvas drawing mechanic. Made more streamlined database creation, but created bug where instead of appending the name of the players in the room, it simply replaces the first one, fukin weird shit

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,135 +6,123 @@
     storageBucket: "partybox-68cd8.appspot.com",
   };
   firebase.initializeApp(config);
-  $(document).ready(function() {
-    step = 1;
+  step = 1;
+  $(function() {
     function makeId() {
     roomId = (0|Math.random()*9e6).toString(36);
-}
-    $("#startGameButt").click(function() {
-      if($("#nameInput").val() != "" && step == 1) {
-      /*makeId();
-      console.log(roomId);
-      $("#firstChoice").animate({opacity: "0"});
-      $("#firstChoice").animate({left: "60%"});
-      */
-      $("#joinGameButt").animate({opacity: "0"});
-      $("#joinGameButt").animate({right: "80%"});
-      //$("#startGameButt").animate({top: "170px"}, {duration: 800, queue: false});
-      $("#nameInput").animate({marginTop: "480px"}, {duration: 800, queue: false});
-      $("#startCont").animate({width: "700px", height: "650px"}, {duration: 800, queue: false});
-      $("#firstChoice").animate({width: "700px", height: "650px"}, {duration: 800, queue: false});
-      $("#deleteImg").animate({opacity: 1}, 1000);
-      $("#drawImg").animate({opacity: 1, zIndex: 1}, {duration: 2000});
-      step = 2;
-      console.log(step);
-      setTimeout(function(){$("#startGameButt").text("Start");}, 100);
     }
-    });
-// Keep everything in anonymous function, called on window load.
-if(window.addEventListener) {
-window.addEventListener('load', function () {
-  var canvas, context, tool;
-
-  function init () {
-    // Find the canvas element.
-    canvas = document.getElementById('drawImg');
-    if (!canvas) {
-      alert('Error: I cannot find the canvas element!');
-      return;
-    }
-
-    if (!canvas.getContext) {
-      alert('Error: no canvas.getContext!');
-      return;
-    }
-
-    // Get the 2D canvas context.
-    context = canvas.getContext('2d');
-    if (!context) {
-      alert('Error: failed to getContext!');
-      return;
-    }
-
-    // Pencil tool instance.
-    tool = new tool_pencil();
-
-    // Attach the mousedown, mousemove and mouseup event listeners.
-    canvas.addEventListener('mousedown', ev_canvas, false);
-    canvas.addEventListener('mousemove', ev_canvas, false);
-    canvas.addEventListener('mouseup',   ev_canvas, false);
-  }
-
-  // This painting tool works like a drawing pencil which tracks the mouse
-  // movements.
-  function tool_pencil () {
-    var tool = this;
-    this.started = false;
-
-    // This is called when you start holding down the mouse button.
-    // This starts the pencil drawing.
-    this.mousedown = function (ev) {
-        context.beginPath();
-        context.strokeStyle = "#df4b26";
-        context.lineWidth = 5;
-        context.moveTo(ev._x, ev._y);
-        tool.started = true;
-    };
-
-    // This function is called every time you move the mouse. Obviously, it only
-    // draws if the tool.started state is set to true (when you are holding down
-    // the mouse button).
-    this.mousemove = function (ev) {
-      if (tool.started) {
-        context.lineTo(ev._x, ev._y);
-        context.stroke();
-      }
-    };
-
-    // This is called when you release the mouse button.
-    this.mouseup = function (ev) {
-      if (tool.started) {
-        tool.mousemove(ev);
-        tool.started = false;
-      }
-    };
-  }
-
-  // The general-purpose event handler. This function just determines the mouse
-  // position relative to the canvas element.
-  function ev_canvas (ev) {
-    if (ev.layerX || ev.layerX == 0) { // Firefox
-      ev._x = ev.layerX;
-      ev._y = ev.layerY;
-    } else if (ev.offsetX || ev.offsetX == 0) { // Opera
-      ev._x = ev.offsetX;
-      ev._y = ev.offsetY;
-    }
-
-    // Call the event handler of the tool.
-    var func = tool[ev.type];
-    if (func) {
-      func(ev);
-    }
-  }
-
-  init();
-  $("#deleteImg").click(function() {
-  if(step == 2) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  }
-  });
-  $("#startGameButt").click(function() {
-    if(step == 2 && $("#startGameButt").text() == "Start") {
-      var dataURL = canvas.toDataURL();
-      console.log(dataURL);
-      makeId();
-      console.log(roomId);
-      firebase.database().ref('rooms/').push({
-        id: roomId,
-        players: 0
+    if(step == 1) {
+      var colArray = ["#F44336", "#E91E63", "#9C27B0", "#3F51B5", "#2196F3", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548"];
+      myColor = colArray[Math.floor(Math.random() * colArray.length)];
+      //Taking player to start a new room
+      $("#startGameButt").click(function() {
+        if($("#nameInput").val() != "" && step == 1) {
+          myName = $("#nameInput").val();
+          console.log("Name: " + myName);
+          $("#startCont").animate({width: 700, height: 700}, {duration: 800, queue: false});
+          $("#firstChoice").animate({width: 700, height: 700}, {duration: 800, queue: false});
+          $("#drawImg").animate({opacity: 1, top: 0, zIndex: 5}, {duration: 800, queue: false});
+          //$("#idInput").animate({marginTop: 150},{duration: 800, queue: false});
+          $("#idInput").animate({zIndex: -2, opacity: 0},{duration: 800, queue: false});
+          $("#joinGameButt").animate({zIndex: -2, opacity: 0},{duration: 800, queue: false});
+          $("#nameInput").animate({marginTop: 450},{duration: 800, queue: false});
+          $("#lobbyList").animate({opacity: 1},{duration: 800, queue: false});
+          step = 2;
+          join = false;
+          makeId();
+          console.log(roomId);
+          console.log("You're now at step " + step);
+          console.log(myColor);
+          step2();
+        }
+      });
+      $("#joinGameButt").click(function() {
+        if($("#nameInput").val() != "" && $("#idInput").val() != "" && step == 1) {
+          myName = $("#nameInput").val();
+          console.log("Name: " + myName);
+          $("#startCont").animate({width: 700, height: 700}, {duration: 800, queue: false});
+          $("#firstChoice").animate({width: 700, height: 700}, {duration: 800, queue: false});
+          $("#drawImg").animate({opacity: 1, top: 0, zIndex: 1}, {duration: 800, queue: false});
+          //$("#idInput").animate({marginTop: 150},{duration: 800, queue: false});
+          $("#idInput").animate({zIndex: -2, opacity: 0},{duration: 800, queue: false});
+          $("#joinGameButt").animate({zIndex: -2, opacity: 0},{duration: 800, queue: false});
+          $("#nameInput").animate({marginTop: 450},{duration: 800, queue: false});
+          $("#lobbyList").animate({opacity: 1},{duration: 800, queue: false});
+          step = 2;
+          join = true;
+          roomId = $("#idInput").val();
+          console.log(roomId);
+          console.log("You're now at step " + step);
+          console.log(myColor);
+          step2();
+        }
       });
     }
-  });
-}, false); }
-  });
+    //Letting user draw a profile image, and entering the room
+    // Only executed our code once the DOM is ready.
+
+	window.onload = function() {
+		// Get a reference to the canvas object
+		var canvas = document.getElementById('drawImg');
+		// Create an empty project and a view for the canvas:
+		paper.setup(canvas);
+    with(paper) {
+		// Create a Paper.js Path to draw a line into it:
+		// Give the stroke a color
+    var tool = new Tool();
+		// Define a mousedown and mousedrag handler
+		tool.onMouseDown = function(event) {
+			path = new Path();
+			path.strokeColor = myColor;
+      path.strokeWidth = 5;
+			path.add(event.point);
+		}
+		tool.onMouseDrag = function(event) {
+			path.add(event.point);
+		}
+  }
+	}
+  function step2() {
+    if(join) {
+      //Checking if the entered roomId exists in the database
+      firebase.database().ref('rooms/').once("value", function(snapshot) {
+        data = snapshot.val();
+        for(i in data) {
+          if(i == roomId) {
+            //if(data.numChildren() <= 3) {
+              firebase.database().ref('rooms/' + roomId + '/playerList/' + myName).update({
+                color: myColor,
+                name: myName
+              });
+            /*} else {
+              console.log("Too many players");
+              break;
+            }*/
+            return true;
+            break;
+          } else {
+            console.log("Should restart now, due to no room being found");
+          }
+        }
+      });
+      joined = true;
+    } else {
+      //creating the room with the give roomId
+      firebase.database().ref('rooms/' + roomId + '/playerList/' + myName).update({
+        color: myColor,
+        name: myName
+      });
+      joined = true;
+    }
+    if(joined) {
+      firebase.database().ref('rooms/' + roomId + '/playerList/').orderByChild("name").limitToLast(12).on("child_added", function(snapshot) {
+        var data = snapshot.val();
+        $("#lobbyList").text("Lobby - " + roomId);
+        var color = data.color;
+        var name = data.name;
+        $("#lobbyList").append("<p style='color: "+color+"'>"+name+"</p>");
+        firebase.database().ref('rooms/' + roomId + '/playerList/' + myName).onDisconnect().remove();
+      });
+    }
+  }
+});
